66837585167129eba106d48cc83cfc5db5e82c47
RZA <rza@wutang.com>
2017-10-12 21:18:02 +0200

    billing - in app Purchase

---
+++ app/src/main/java/at/android/princesslea/Donate.java
@@ -0,0 +1,305 @@
+package at.android.princesslea;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.billingclient.api.BillingClient;
+
+import at.android.princesslea.util.IabBroadcastReceiver;
+import at.android.princesslea.util.IabBroadcastReceiver.IabBroadcastListener;
+import at.android.princesslea.util.IabHelper;
+import at.android.princesslea.util.IabHelper.IabAsyncInProgressException;
+import at.android.princesslea.util.IabResult;
+import at.android.princesslea.util.Inventory;
+import at.android.princesslea.util.Purchase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Donate extends Activity implements IabBroadcastListener,
+        OnClickListener {
+
+    static final String TAG = "Donate";
+
+    // Does the user have the premium upgrade?
+    boolean mIsPremium = false;
+
+    // Tracks the currently owned infinite gas SKU, and the options in the Manage dialog
+    String mInfiniteGasSku = "";
+    String mFirstChoiceSku = "";
+    String mSecondChoiceSku = "";
+
+    // SKUs for our products: the premium upgrade (non-consumable) and gas (consumable)
+    static final String SKU_PREMIUM = "premium";
+
+    // (arbitrary) request code for the purchase flow
+    static final int RC_REQUEST = 10001;
+
+    // The helper object
+    IabHelper mHelper;
+
+    // Provides purchase notification while this app is running
+    IabBroadcastReceiver mBroadcastReceiver;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+
+        String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuELbZCKGx6cY7+ajWXGlvAKLZMOeAeOThGd+eJRTxSSXzWhWk0O6byojkgtBeFC09S25rZi4riYbIHr3lkmCHMH8FewhIurLiAJoTeM4w5k4cCUkH1y2BZdHSgq0OKq/W8lfIMTM9JoCGd493hgV9PIsoasLArfahVygB9ZPXP0xHJIjrFcKN5eoqHGVpCLneMdMVdEqL4/RrHpX9tJD+S3fMxs+puxLznsO34bW8NPcgtkdsRB9aXeMz92DHSTxj92XmNtXBCds+adYCGq18sQGc6K3VdCzI4sD7DOalDneFjLD9T4+1QJ7yVRQ1j8uFhqs+ogGZbdxwJ8o09szAQIDAQAB";
+
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+
+        mHelper.enableDebugLogging(true);
+
+        Log.d(TAG, "Starting setup.");
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    complain("Problem setting up in-app billing: " + result);
+                    return;
+                }
+
+                // Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null) return;
+
+                // Important: Dynamically register for broadcast messages about updated purchases.
+                // We register the receiver here instead of as a <receiver> in the Manifest
+                // because we always call getPurchases() at startup, so therefore we can ignore
+                // any broadcasts sent while the app isn't running.
+                // Note: registering this listener in an Activity is a bad idea, but is done here
+                // because this is a SAMPLE. Regardless, the receiver must be registered after
+                // IabHelper is setup, but before first call to getPurchases().
+                mBroadcastReceiver = new IabBroadcastReceiver(Donate.this);
+                IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
+                registerReceiver(mBroadcastReceiver, broadcastFilter);
+
+                // IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(TAG, "Setup successful. Querying inventory.");
+                try {
+                    mHelper.queryInventoryAsync(mGotInventoryListener);
+                } catch (IabAsyncInProgressException e) {
+                    Log.d(TAG, "onIabSetupFinished: Error querying inventory. Another async operation in progress.");
+                }
+            }
+        });
+    }
+
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (mHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure()) {
+                complain("Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+            /*
+             * Check for items we own. Notice that for each purchase, we check
+             * the developer payload to see if it's correct! See
+             * verifyDeveloperPayload().
+             */
+
+            // Do we have the premium upgrade?
+            Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
+            mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+            Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+
+            updateUi();
+            setWaitScreen(false);
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    @Override
+    public void receivedBroadcast() {
+        // Received a broadcast notification that the inventory of items has changed
+        Log.d(TAG, "Received broadcast notification. Querying inventory.");
+        try {
+            mHelper.queryInventoryAsync(mGotInventoryListener);
+        } catch (IabAsyncInProgressException e) {
+            complain("Error querying inventory. Another async operation in progress.");
+        }
+    }
+
+
+
+    @Override
+    public void onClick(DialogInterface dialog, int id) {
+        if (id == 0 /* First choice item */) {
+            Log.d(TAG, "Upgrade button clicked; launching purchase flow for upgrade.");
+            setWaitScreen(true);
+
+        /* TODO: for security, generate your payload here for verification. See the comments on
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
+         *        an empty string, but on a production app you should carefully generate this. */
+            String payload = "";
+
+            try {
+                mHelper.launchPurchaseFlow(this, SKU_PREMIUM, RC_REQUEST,
+                        mPurchaseFinishedListener, payload);
+            } catch (IabAsyncInProgressException e) {
+                complain("Error launching purchase flow. Another async operation in progress.");
+                setWaitScreen(false);
+            }
+
+        } else  {
+            // There are only four buttons, this should not happen
+            Log.e(TAG, "Unknown button clicked in subscription dialog: " + id);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+        if (mHelper == null) return;
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            // not handled, so handle it ourselves (here's where you'd
+            // perform any handling of activity results not related to in-app
+            // billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        } else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+
+    /**
+     * Verifies the developer payload of a purchase.
+     */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
+
+            if (result.isFailure()) {
+                complain("Error purchasing: " + result);
+                setWaitScreen(false);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                complain("Error purchasing. Authenticity verification failed.");
+                setWaitScreen(false);
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_PREMIUM)) {
+                // bought the premium upgrade!
+                Log.d(TAG, "Purchase is premium upgrade. Congratulating user.");
+                alert("Thank you for upgrading to premium!");
+                mIsPremium = true;
+                updateUi();
+                setWaitScreen(false);
+            }
+
+        }
+    };
+
+
+    // updates UI to reflect model
+    public void updateUi() {
+//        // update the car color to reflect premium status or lack thereof
+//        ((ImageView)findViewById(R.id.free_or_premium)).setImageResource(mIsPremium ? R.drawable.premium : R.drawable.free);
+//
+//        // "Upgrade" button is only visible if the user is not premium
+//        findViewById(R.id.upgrade_button).setVisibility(mIsPremium ? View.GONE : View.VISIBLE);
+//
+//        ImageView infiniteGasButton = (ImageView) findViewById(R.id.infinite_gas_button);
+//        if (mSubscribedToInfiniteGas) {
+//            // If subscription is active, show "Manage Infinite Gas"
+//            infiniteGasButton.setImageResource(R.drawable.manage_infinite_gas);
+//        } else {
+//            // The user does not have infinite gas, show "Get Infinite Gas"
+//            infiniteGasButton.setImageResource(R.drawable.get_infinite_gas);
+//        }
+//
+//        // update gas gauge to reflect tank status
+//        if (mSubscribedToInfiniteGas) {
+//            ((ImageView)findViewById(R.id.gas_gauge)).setImageResource(R.drawable.gas_inf);
+//        }
+//        else {
+//            int index = mTank >= TANK_RES_IDS.length ? TANK_RES_IDS.length - 1 : mTank;
+//            ((ImageView)findViewById(R.id.gas_gauge)).setImageResource(TANK_RES_IDS[index]);
+//        }
+    }
+
+
+    // Enables or disables the "please wait" screen.
+    void setWaitScreen(boolean set) {
+//        findViewById(R.id.screen_main).setVisibility(set ? View.GONE : View.VISIBLE);
+//        findViewById(R.id.screen_wait).setVisibility(set ? View.VISIBLE : View.GONE);
+    }
+
+
+    void complain(String message) {
+        Log.e(TAG, "**** TrivialDrive Error: " + message);
+        alert("Error: " + message);
+    }
+
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(TAG, "Showing alert dialog: " + message);
+        bld.create().show();
+    }
+}
