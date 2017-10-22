290d48e5f32c5152b4fca0b8a33c670d14f61895
RZA <rza@wutang.com>
2017-10-22 20:40:54 +0200

    install wizzard + new string resources
    cleanup of main

--- app/src/main/java/at/android/lovebubble/MainActivity.java
+++ app/src/main/java/at/android/lovebubble/MainActivity.java
@@ -5,36 +5,99 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    public final static int REQUEST_CODE = -1010101;
+    public final static int REQUEST_CODE = 101;
+    private static final String TAG = "MainActivity";
+    private Button button;
+    private TextView textViewTitle, textViewMain, textViewStatusbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-
-
-        checkDrawOverlayPermission();
 
         if (Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(MainActivity.this, FloatingFaceBubbleService.class);
-            startService(intent);
+            startService(new Intent(MainActivity.this, FloatingFaceBubbleService.class));
+            Toast.makeText(this, R.string.doubleTapSettings, Toast.LENGTH_LONG).show();
+            finish();
+
+        } else {
+            setContentView(R.layout.activity_main);
+            button = (Button) findViewById(R.id.buttonMain);
+            button.setText(R.string.grantPermissionButton);
+            button.setOnClickListener(this);
+
+            textViewTitle = (TextView) findViewById(R.id.textViewTitle);
+            textViewTitle.setText(R.string.textviewTitleWelcome);
+
+            textViewMain = (TextView) findViewById(R.id.textViewMain);
+            textViewMain.setText(R.string.grantPermission);
+
+            textViewStatusbar = (TextView) findViewById(R.id.textViewStatusbar);
+            textViewStatusbar.setText(R.string.status_step1);
+
+            //t = Toast.makeText(this, R.string.grantPermission, Toast.LENGTH_LONG);
+            //t.show();
         }
 
-        finish();
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /* check if received result code is equal our requested code for draw permission */
+        if (requestCode == REQUEST_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+
+                startService(new Intent(MainActivity.this, FloatingFaceBubbleService.class));
+
+                textViewTitle.setText(R.string.main_title_success);
+                textViewMain.setText(R.string.successDoubleTapForSettings);
+                button.setText(R.string.gotIt);
+
+                textViewStatusbar.setText(R.string.status_step2);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        if (getString(R.string.grantPermissionButton).equals(buttonText)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE);
+
+        } else if (getResources().getString(R.string.gotIt).equals(buttonText)) {
+            finish();
+        } else {
+            Log.e(TAG, "onClick: Received unhandled click event"+b.getText());
+        }
+
+    }
+
+
+    /*
+    public boolean checkDrawOverlayPermission() {
+        // check if we already  have permission to draw over other apps
+    }
+    */
+
+    /*
+    @Override
     protected void onResume() {
-//        Bundle bundle = getIntent().getExtras();
-//        if(bundle != null && bundle.getString("LAUNCH").equals("YES")) {
-//            startService(new Intent(MainActivity.this, FloatingFaceBubbleService.class));
-//        }
+    //        Bundle bundle = getIntent().getExtras();
+    //        if(bundle != null && bundle.getString("LAUNCH").equals("YES")) {
+    //            startService(new Intent(MainActivity.this, FloatingFaceBubbleService.class));
+    //        }
 
         if (Settings.canDrawOverlays(this)) {
             Intent intent = new Intent(MainActivity.this, FloatingFaceBubbleService.class);
@@ -43,7 +106,8 @@ public class MainActivity extends Activity {
 
         super.onResume();
     }
-/*
+    */
+    /*
     @Override
     protected void onPause() {
 
@@ -51,34 +115,8 @@ public class MainActivity extends Activity {
 
         super.onPause();
     }
-*/
-
-    public void checkDrawOverlayPermission() {
-        // check if we already  have permission to draw over other apps */
-        if (!Settings.canDrawOverlays(getApplicationContext())) {
-            // if not construct intent to request permission */
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            // request permission via start activity for result */
-            startActivityForResult(intent, REQUEST_CODE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        /* check if received result code
-         is equal our requested code for draw permission  */
-
-
-
-        if (requestCode == REQUEST_CODE) {
-            if (Settings.canDrawOverlays(this)) {
-                Intent intent = new Intent(MainActivity.this, FloatingFaceBubbleService.class);
-                startService(intent);
-            }
-        }
-    }
-
+    */
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -99,7 +137,5 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-
+    */
 }
